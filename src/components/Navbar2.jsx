55f8843c33396f69824bb72eb1a1a270321d5c57
cassnova <daniel.rojascasanova@gmail.com>
2022-12-12 22:19:58 -0300

    working in nav component to crea planeta

---
+++ src/components/Navbar2.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar2.css";
+import Logo from "../logo-prueba.svg";
+
+const Nav = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      color: isActive ? "var(--tertiary)" : "",
+      textDecoration: isActive ? "underline" : "none",
+    };
+  };
+  return (
+    <nav className="nav-container">
+      <NavLink to="/">
+        <img src={Logo} alt="Logo LarnU" className="logo" />
+      </NavLink>
+
+      <ul className={clicked ? "active" : null}>
+        <li>
+          <NavLink style={navLinkStyles} to="/">
+            Inicio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navLinkStyles} to="/personas">
+            Personas
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navLinkStyles} to="/objetivos">
+            Objetivos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navLinkStyles} to="/portafolio">
+            Portafolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={navLinkStyles} to="/contacto">
+            Contacto
+          </NavLink>
+        </li>
+      </ul>
+
+      <div id="mobile">
+        <i
+          id="bar"
+          className={`fas fa-bars ${clicked ? "fas fa-times" : "fas fa-bars"}`}
+          onClick={() => setClicked(!clicked)}
+        ></i>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
