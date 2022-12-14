6878efe688c8d2d596af20c5b068e77551c820b8
cassnova <daniel.rojascasanova@gmail.com>
2022-12-14 20:17:35 -0300

    react router done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Inicio from "./pages/Inicio/Inicio";
+import Objetivos from "./pages/Objetivos/Objetivos";
+import Personas from "./pages/Personas/Personas";
+import Portafolio from "./pages/Portafolio/Portafolio";
+import Contacto from "./pages/Contacto/Contacto";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Inicio />
+      <Routes>
+        <Route exact path="/" element={<Inicio />} />
+        <Route exact path="/objetivos" element={<Objetivos />} />
+        <Route exact path="/personas" element={<Personas />} />
+        <Route exact path="/portafolio" element={<Portafolio />} />
+        <Route exact path="/contacto" element={<Contacto />} />
+      </Routes>
     </div>
   );
 }
