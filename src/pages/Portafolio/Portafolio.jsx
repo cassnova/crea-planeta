bc6779a195d8bd27d3ba698514cc7f83c130b144
cassnova <daniel.rojascasanova@gmail.com>
2022-12-16 22:59:09 -0300

    added text to portafolio page

--- src/pages/Portafolio/Portafolio.jsx
+++ src/pages/Portafolio/Portafolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../../components/Navbar2";
 import { motion as m } from "framer-motion";
+import "./Portafolio.css";
 
 const Portafolio = () => {
   return (
@@ -11,7 +12,8 @@ const Portafolio = () => {
       transition={{ duration: 0.5, ease: "easeOut" }}
     >
       <Nav />
-      <h2>Portafolio</h2>
+      <h2>PORTAFOLIO</h2>
+      <hr />
     </m.div>
   );
 };
