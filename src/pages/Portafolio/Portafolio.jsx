8ae2959ce11c2044ce5113fa4fa7be7784b79367
cassnova <daniel.rojascasanova@gmail.com>
2022-12-18 13:03:35 -0300

    added elements and content to portafolio page

--- src/pages/Portafolio/Portafolio.jsx
+++ src/pages/Portafolio/Portafolio.jsx
@@ -14,6 +14,23 @@ const Portafolio = () => {
       <Nav />
       <h2>PORTAFOLIO</h2>
       <hr />
+      <section>
+        <div className="explora-container">
+          <div>
+            <h2>EXPLORA EN 360°</h2>
+          </div>
+          <div className="explora-container__locaciones">
+            <h3>ANTÁRTICA</h3>
+            <h3>ATACAMA</h3>
+            <h3>RAPA NUI</h3>
+            <h3>JUAN FERNÁNDEZ</h3>
+          </div>
+        </div>
+        <div className="molecula-container">
+          <h2>104.5°</h2>
+          <h3>MOLÉCULA DE LA VIDA</h3>
+        </div>
+      </section>
     </m.div>
   );
 };
