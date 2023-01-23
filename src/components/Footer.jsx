b8aa8f26a89843d63e8b551e2d68870e30bc56ff
cassnova <daniel.rojascasanova@gmail.com>
2023-01-23 20:58:15 -0300

    added insta logo and styles to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Footer.css";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>Todos los derechos reservados.</p>
-      <p>© 2022 Desarrollo y Diseño Web por Agencia DRC</p>
+      <div className="footer-container__insta">
+        <p>Visita nuestro Instagram</p>
+        <a href="https://www.instagram.com/crea.planeta/" target="__blank">
+          <AiOutlineInstagram className="logo-insta-footer" />
+        </a>
+      </div>
+      <div>
+        <p>Todos los derechos reservados.</p>
+        <p>
+          © 2022 Desarrollo y Diseño Web por{" "}
+          <a
+            href="https://www.instagram.com/agencia.drc/"
+            className="url-agencia-drc"
+            target="__blank"
+          >
+            Agencia DRC
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
